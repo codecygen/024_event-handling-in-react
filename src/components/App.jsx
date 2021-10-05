99ee622b331b90c540627d89b1ad445c29aae3b9
gingercook <vahitaras@gmail.com>
2021-10-04 20:55:16 -0400

    click event greeting will be added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,9 +4,10 @@ import React, {useState} from "react";
 const App = () => {
 
 
-  const [headingText, setHeadingText] = useState('Hello');
+  const [headingText, setHeadingText] = useState('No Mouse Event!');
 
   const [buttonColor, setButtonColor] = useState('white');
+  const [name, setName] = useState('')
 
   const handleClick = () => {
     setHeadingText('Clicked!');
@@ -24,13 +25,26 @@ const App = () => {
 
   const handleChange = (e) => {
     console.log(e.target.value);
+    setName(e.target.value);
   }
 
+  // In HTML, elements are responsible of handling their own state.
+  // For example, if you type in an input element, the value will be
+  // equal to the one that you already typed.
+
+  // In React, however, you have to set the value property to the 
+  // Thing you typed manually.
 
   return (
     <div className="container">
+      <h1>Hello {name}</h1>
+      <h1>You typed {name}</h1>
       <h1>{headingText}</h1>
-      <input type="text" placeholder="What's your name?" onChange={handleChange} />
+      <input 
+      type="text" 
+      placeholder="What's your name?" 
+      onChange={handleChange}
+      value={name} />
       
       <button 
         style={{backgroundColor: buttonColor}} 
